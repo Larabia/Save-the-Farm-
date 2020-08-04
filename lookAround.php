def212706b8c92564baa628f499bb43dc098c434
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-08-04 19:49:08 -0300

    starting lookAround function

---
+++ lookAround.php
@@ -0,0 +1,24 @@
+<?php
+  // Describe current location
+	function lookAround(){
+	 
+   global $location, $wearing_glasses, $wearing_contacts, $moved_cupboard;
+
+   if ($wearing_glasses xor $wearing_contacts){
+
+   } else {
+     echo "It's really hard to make out any details...\n";
+   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
