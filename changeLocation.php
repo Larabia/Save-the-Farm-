1c062e4b1187aba843c5a1298ad351cf1b5bec35
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-08-04 19:44:56 -0300

    changeLocation completed

--- changeLocation.php
+++ changeLocation.php
@@ -4,7 +4,43 @@ function changeLocation(){
   global $location;
 
   echo "Where do you want to go?\n";
-  $response = strtolower(readline(">>"));
+  $comand = strtolower(readline(">>"));
+
+  switch ($comand){
+    
+    case $comand==="bathroom":
+      if ($location === "kitchen"){
+        echo "You go to: bathroom.\n";
+        $location = "bathroom";
+      }else {
+        echo "You can't go directly to there from $location. Try going somewhere else first.\n";
+      }
+    break;
+
+    case $comand==="woods":
+    if ($location === "kitchen"){
+        echo "You follow the winding path, shivering as you make your way deep into the Terror Woods.\n";
+        $location = "woods";
+      }else {
+        echo "You can't go directly to there from $location. Try going somewhere else first.\n";
+      }
+    break;
+
+    case $comand==="kitchen":
+    if ($location === "woods"||$location === "bathroom"){
+        echo "You go to: kitchen.\n";
+        $location = "kitchen";
+      }else {
+        echo "You can't go directly to there from $location. Try going somewhere else first.\n";
+      }
+    break;
+
+    default:
+    echo "That doesn't make sense. Are you confused? Try 'look around'.\n";
+    break;
+  
+  }
+
 
 
   
