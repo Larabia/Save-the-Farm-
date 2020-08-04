05f3e91a35ff8a3be938cd6b4cb9d9af69816af8
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-08-04 19:54:23 -0300

    location case kitchen added

--- lookAround.php
+++ lookAround.php
@@ -6,6 +6,20 @@
 
    if ($wearing_glasses xor $wearing_contacts){
 
+     switch($location){
+
+       case $location==="kitchen":
+       echo "This kitchen comes with all the tools and ingredients needed to cook mushroom soup--- except the mushrooms!\n\nFrom here, you see the door to the *bathroom* and the backdoor, which leads to the *woods*.\n\n";
+       if ($moved_cupboard){
+         echo "The cupboard has been moved aside, and reveals a safe built in to the wall.\n";
+       } else {
+         echo "Also, there's a conspicuously large cupboard against a peculiarly worn piece of the wall.\n";
+       }
+       break;
+
+       case $location==="kitchen":
+     }
+
    } else {
      echo "It's really hard to make out any details...\n";
    }
@@ -13,6 +27,15 @@
   
   
   
+  
+  
+  
+  
+  
+  
+  
+  
+}
   
   
   
