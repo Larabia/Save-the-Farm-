ab9bf75b773f3e41710e32a4815208cb3ac71ac9
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-08-04 19:00:44 -0300

    starting changeLocation function

---
+++ changeLocation.php
@@ -0,0 +1,14 @@
+<?php
+  // Change player location
+function changeLocation(){	
+  global $location;
+
+  echo "Where do you want to go?\n";
+  $response = strtolower(readline(">>"));
+
+
+  
+  
+  
+  
+}
